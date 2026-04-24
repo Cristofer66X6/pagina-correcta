bfadcf7ca2e8ed668177b4d86923e828de5aa64f
Cristofer <alberto.c666c@gmail.com>
2026-04-23 22:55:19 -0600

    fix cors render

--- backend/server.js
+++ backend/server.js
@@ -27,36 +27,12 @@ app.use(express.json());
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => {
-
-    const { email } = req.body;
-
-    // 🔥 buscar usuario para obtener nombre
-    const user = await User.findOne({ email });
-
-    // 🔥 fallback por si algo falla
-    let nombre = user?.nombre || "usuario";
-
-    // 🔥 limpiar nombre
-    nombre = nombre
-      .toLowerCase()
-      .trim()
-      .replace(/\s+/g, "_")
-      .replace(/[^\w]/g, "");
-
-    // 🔥 limpiar email
-    const safeEmail = email
-      .toLowerCase()
-      .replace(/[@.]/g, "_");
-
-    // 🔥 carpeta final
-    const folderName = `pdfs/${nombre}_${safeEmail}`;
-
     return {
-      folder: folderName, // 👈 AQUÍ ESTÁ LA MAGIA
+      folder: "pdfs",
 
-      resource_type: "auto",
-      type: "upload",
-      access_mode: "public",
+      resource_type: "auto",   // 🔥 necesario para PDF
+      type: "upload",         // 🔥 público
+      access_mode: "public",  // 🔥 evita 401
 
       public_id: Date.now() + "-" + file.originalname,
 
